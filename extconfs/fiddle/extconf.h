7197be143d1f446ef7dc424447cf0e7edf1713d9
Thomas A <CuriousTommy@users.noreply.github.com>
2020-03-22 09:08:02 -0700

    Add fixes for libffi code

--- extconfs/fiddle/extconf.h
+++ extconfs/fiddle/extconf.h
@@ -1,7 +1,7 @@
 #ifndef EXTCONF_H
 #define EXTCONF_H
 #define HAVE_FFI_FFI_H 1
-#define USE_HEADER_HACKS 1
+// #define USE_HEADER_HACKS 1
 #define HAVE_SYS_MMAN_H 1
 #define HAVE_DLFCN_H 1
 #define HAVE_DLOPEN 1
